b51a403e587cb8436a139dce51f1ec92548b83de
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-25 11:24:01 +0700

    update user api

--- api/users.ts
+++ api/users.ts
@@ -1,16 +1,29 @@
-import axios from 'axios';
+import axios, {AxiosResponse, AxiosError} from 'axios';
+import { ErrorResponse } from './error';
+import { useQuery } from 'react-query';
 
 export interface User {
   id: string;
   name: string;
   email: string;
-  avatar: string | null;
+}
+
+export interface GetUserResponse {
+  data: { users: Array<User> };
+  message: string;
+  status: string;
 }
 
 export function fetchUsers() {
   return axios({
-    url: 'http://localhost:7002/users',
+    url: 'http://localhost:7002/api/v1/users',
     method: 'get',
     withCredentials: true,
+  });
+}
+
+export function useGetUsers() {
+  return useQuery<AxiosResponse<GetUserResponse>, AxiosError<ErrorResponse>>('users', fetchUsers, {
+    retry: false,
   });
 }
