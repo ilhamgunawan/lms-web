24903d9608f12fabe9dc147620172b31e0bc1209
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-10 11:17:33 +0700

    New Layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,21 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document';
-import { createGetInitialProps } from '@mantine/next';
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyles, createStylesServer } from '@mantine/next';
 
-const getInitialProps = createGetInitialProps();
+// optional: you can provide your cache as a first argument in createStylesServer function
+const stylesServer = createStylesServer();
 
-class MyDocument extends Document {
-  static getInitialProps = getInitialProps;
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
 
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    // Add your app specific logic here
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
+      ],
+    };
   }
 }
-
-export default MyDocument;
