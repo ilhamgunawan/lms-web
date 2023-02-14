e14b68bb2e3e1c7dc57817cdc018254b461f9024
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-14 16:04:40 +0700

    Create axios fetch service

---
+++ services/fetch.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const token = global.window ? window.localStorage.getItem('token') ?? '' : '';
+
+const fetchClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_ENDPOINT,
+  responseType: 'json',
+  headers: {
+    'Authorization': token,
+  },
+});
+
+export { fetchClient };
